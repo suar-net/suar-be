b66addb97ab1757475356b7bcfb617a6f7a762f2
yodh4 <cyrilus2004@gmail.com>
2025-08-03 18:49:09 +0700

    Add JWT dependency

--- internal/model/dto.go
+++ internal/model/dto.go
@@ -3,9 +3,10 @@ package model
 import (
 	"encoding/json"
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
-// DTO for incoming HTTP requests
 // Change incoming request body from JSON to http request format
 type DTORequest struct {
 	Method  string              `json:"method" validate:"required"`
@@ -15,7 +16,6 @@ type DTORequest struct {
 	Timeout int                 `json:"timeout" validate:"gte=0,lte=90000"` // 0 means default, max 90s
 }
 
-// DTO for outgoing JSON responses
 // Change incoming http response from complex object to simplified version
 type DTOResponse struct {
 	StatusCode int                 `json:"status_code"`
@@ -25,4 +25,27 @@ type DTOResponse struct {
 	Headers    map[string][]string `json:"headers"`
 	Body       []byte              `json:"body,omitempty"`
 	Error      string              `json:"error,omitempty"`
+}
+
+type DTOUserRegisterRequest struct {
+	Username string `json:"username" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type DTOLoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type DTOLoginResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
+type Claims struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	jwt.RegisteredClaims
 }
